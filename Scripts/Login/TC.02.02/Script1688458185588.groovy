a2711734f5719545a4b8ea783a3255b6a72fc2b8
ASUS <ASUS@DESKTOP-4KJ2CC8>
2023-07-04 15:44:07 +0700

    TC2

---
+++ Scripts/Login/TC.02.02/Script1688458185588.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://cv-me.metrodataacademy.id')
+
+WebUI.click(findTestObject('Login/Page_Beranda  CV-ME/a_Login'))
+
+WebUI.setText(findTestObject('Login/Page_Beranda  CV-ME/input_Masuk ke akun Anda_email'), '')
+
+WebUI.setEncryptedText(findTestObject('Login/Page_Beranda  CV-ME/input_Masuk ke akun Anda_password'), 'Vo9yPSa6Iy9LZq50j2r1GA==')
+
+WebUI.click(findTestObject('Login/Page_Beranda  CV-ME/button_MASUK'))
+
+WebUI.verifyElementVisible(findTestObject('Login/Page_Beranda  CV-ME/small_Email tidak boleh kosong'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.closeBrowser()
+
